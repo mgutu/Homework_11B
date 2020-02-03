255c9faf995a769e68f48a4f831854baa439cae7
olgaslipenski <45373349+olgaslipenski@users.noreply.github.com>
2020-02-03 14:34:53 +0200

    Add files via upload

---
+++ slipenskiolga 11B.pas
@@ -0,0 +1,11 @@
+﻿Program TemaPentruAcasa;
+var a, b, x, y, D:real;
+function distanta (a1, b1, x1, y1:real):real;
+begin
+distanta:=sqrt(sqr(a1-x1)+sqr(b1-y1));
+end;
+begin
+readln (a,b,x,y);
+D:=distanta (a,b,x,y);
+writeln ('distanta dintre coordonate=', D);
+end.
